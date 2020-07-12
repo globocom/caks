2a0f984cab953b173f412e41557bc0a9efd5c928
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-07-12 15:11:31 -0300

    Implements ConvertToIntegerMiliCores in cpu.go

--- pkg/resources/cpu.go
+++ pkg/resources/cpu.go
@@ -1,7 +1,29 @@
 package resources
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func ConvertIntegerToStringMilicores(value int)string {
 	return fmt.Sprintf("%vm",value)
+}
+
+func ConvertToIntegerMiliCores(value string)(int, error) {
+
+	miliCores, err := strconv.Atoi(value)
+
+	if err != nil {
+		if strings.HasSuffix(value,"m"){
+			miliCores, err = strconv.Atoi(strings.Split(value,"m")[0])
+			if err != nil {
+				return 0, err
+			}
+		}
+	}else{
+		miliCores = miliCores * 1000
+	}
+
+	return miliCores, nil
 }
