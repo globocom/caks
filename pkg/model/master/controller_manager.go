e2d601dbeaab88af586cedcfbcd80750b6a73671
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-07-14 16:03:54 -0300

    Define buildCommands in controller manager

--- pkg/model/master/controller_manager.go
+++ pkg/model/master/controller_manager.go
@@ -13,4 +13,21 @@ type ControllerManager struct {
 	resourceRequirements corev1.ResourceRequirements
 }
 
-
+func (controllerManager *ControllerManager) buildCommands()[]string{
+	return []string{
+		controllerManager.applicationName,
+		printFlag("address", "0.0.0.0"),
+		printFlag("cluster-cidr",controllerManager.clusterCIDRS),
+		printFlag("allocate-node-cidrs", true),
+		printFlag("cluster-name",controllerManager.clusterName),
+		printFlag("cluster-signing-cert-file","/var/lib/kubernetes/ca/ca.pem"),
+		printFlag("cluster-signing-key-file", "/var/lib/kubernetes/ca/ca-key.pem"),
+		printFlag("kubeconfig","/var/lib/kubernetes/kube-controller-manager.kubeconfig"),
+		printFlag("leader-elect", true),
+		printFlag("root-ca-file","/var/lib/kubernetes/ca/ca.pem"),
+		printFlag("service-account-private-key-file","/var/lib/kubernetes/service-account-key.pem"),
+		printFlag("service-cluster-ip-range",controllerManager.serviceClusterIPRange),
+		printFlag("use-service-account-credentials", true),
+		printFlag("v", 2),
+	}
+}
