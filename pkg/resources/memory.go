420138d8e1ee3087e419a2dbad4284f462c43414
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-07-28 15:22:43 -0300

    Implement ConvertToMebiBytes in memory.go

--- pkg/resources/memory.go
+++ pkg/resources/memory.go
@@ -7,6 +7,27 @@ import (
 	"strings"
 )
 
+func ConvertToMebiBytes(value string)(int,error){
+
+	if strings.HasSuffix(value,"Gi"){
+		valueInGibiByte, err := convertMemoryValueToInteger(value, "Gi")
+		if err != nil {
+			return 0,err
+		}
+		valueInMebiByte := valueInGibiByte * 1024
+		return valueInMebiByte, nil
+
+	} else if strings.HasSuffix(value, "Mi"){
+		valueInMebiByte, err := convertMemoryValueToInteger(value, "Mi")
+		if err != nil {
+			return 0, err
+		}
+		return valueInMebiByte, nil
+	}
+
+	return 0, errors.New("memory unit not supported")
+}
+
 func ConvertIntegerToStringMebiBytes(value int)string{
 	return fmt.Sprintf("%dMi",value)
 }
