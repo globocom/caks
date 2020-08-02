5ece25da6c65150490e45138db3157524e6b1334
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-08-02 16:09:35 -0300

    Create buildCommands in api server

--- pkg/model/master/api_server.go
+++ pkg/model/master/api_server.go
@@ -1,6 +1,9 @@
 package master
 
-import corev1 "k8s.io/api/core/v1"
+import (
+	corev1 "k8s.io/api/core/v1"
+	"strings"
+)
 
 type apiServer struct {
 	image string
@@ -9,4 +12,43 @@ type apiServer struct {
 	advertiseAddress string
 	admissionPlugins []string
 	resourceRequirements corev1.ResourceRequirements
+}
+
+func (apiServer *apiServer) buildCommands()[]string{
+
+	printAdmissionPlugins := func ()string{
+		return strings.Join(apiServer.admissionPlugins,",")
+	}
+
+	return []string{
+		apiServer.applicationName,
+		printFlag("advertise-address",apiServer.advertiseAddress),
+		printFlag("allow-privileged",true),
+		printFlag("apiserver-count", 1),
+		printFlag("audit-log-maxage",30),
+		printFlag("audit-log-maxbackup",3),
+		printFlag("audit-log-maxsize",100),
+		printFlag("audit-log-path=","/var/log/audit.log"),
+		printFlag("authorization-mode","Node,RBAC"),
+		printFlag("bind-address","0.0.0.0"),
+		printFlag("client-ca-file","/var/lib/kubernetes/ca/ca.pem"),
+		printFlag("enable-admission-plugins", printAdmissionPlugins()),
+		printFlag("etcd-cafile","/var/lib/kubernetes/ca/ca.pem"),
+		printFlag("etcd-certfile","/var/lib/kubernetes/kubernetes.pem"),
+		printFlag("etcd-keyfile","/var/lib/kubernetes/kubernetes-key.pem"),
+		printFlag("etcd-servers","https://161.35.116.213:2379"),
+		printFlag("event-ttl","1h"),
+		printFlag("encryption-provider-config","/var/lib/kubernetes/encryption/encryption-config.yaml"),
+		printFlag("kubelet-certificate-authority","/var/lib/kubernetes/ca/ca.pem"),
+		printFlag("kubelet-client-certificate","/var/lib/kubernetes/kubernetes.pem"),
+		printFlag("kubelet-client-key","/var/lib/kubernetes/kubernetes-key.pem"),
+		printFlag("kubelet-https",true),
+		printFlag("runtime-config","api/all"),
+		printFlag("service-account-key-file","/var/lib/kubernetes/service-account.pem"),
+		printFlag("service-cluster-ip-range", apiServer.serviceClusterIPRange),
+		printFlag("service-node-port-range","30000-32767"),
+		printFlag("tls-cert-file","/var/lib/kubernetes/kubernetes.pem"),
+		printFlag("tls-private-key-file","/var/lib/kubernetes/kubernetes-key.pem"),
+		printFlag("v",2),
+	}
 }
